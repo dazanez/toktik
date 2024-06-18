8af7f38eba3da9a8324fb9b6b5b8ec1eca0828a0
dazanez <dasl.672004@gmail.com>
2024-06-18 02:02:09 -0500

    Video datasources and repositories added

---
+++ lib/infrasctructure/datasources/local_video_datasource_impl.dart
@@ -0,0 +1,22 @@
+import 'package:toktik/domain/datasources/video_post_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/infrasctructure/models/local_video_model.dart';
+import 'package:toktik/shared/data/local_video_posts.dart';
+
+class LocalVideoPostsDataSource implements VideoPostDatasource {
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userId) {
+    // TODO: implement getFavoriteVideosByUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) async {
+    await Future.delayed(const Duration(seconds: 2));
+    List<VideoPost> newVideos = videoPosts
+        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+        .toList();
+
+    return newVideos;
+  }
+}
